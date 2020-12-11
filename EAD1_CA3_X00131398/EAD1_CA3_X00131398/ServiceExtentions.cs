fb5669c9495b7e8f501ac046b703e7217fbe8b2b
tsam137 <tobasamuel137@gmail.com>
2020-12-11 14:05:49 +0000

    Changes to the Token
    Cors still needs fix

--- EAD1_CA3_X00131398/EAD1_CA3_X00131398/ServiceExtentions.cs
+++ EAD1_CA3_X00131398/EAD1_CA3_X00131398/ServiceExtentions.cs
@@ -18,12 +18,12 @@ namespace EAD1_CA3_X00131398
         public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Add("Bearer-Token", "d8KcVpyqLxtuA57QLKLA_88oSUFTqaOIdIUqWgnF4S3ialJu2sSQPqZfGRtpmMT5MvNXDUtp0jP-awY8cN4DZaP6y0qzD0I21fwtT4gZRItMFHYDJHZCpdeYDyLOX3Yx");
+            request.Headers.Add("X-Auth-Token", "d8KcVpyqLxtuA57QLKLA_88oSUFTqaOIdIUqWgnF4S3ialJu2sSQPqZfGRtpmMT5MvNXDUtp0jP-awY8cN4DZaP6y0qzD0I21fwtT4gZRItMFHYDJHZCpdeYDyLOX3Yx");
 
 
             var response = await httpClient.SendAsync(request);
             response.Headers.Add("Access-Control-Allow-Origin", "*");
-            response.Headers.Add("Bearer-Token", "d8KcVpyqLxtuA57QLKLA_88oSUFTqaOIdIUqWgnF4S3ialJu2sSQPqZfGRtpmMT5MvNXDUtp0jP-awY8cN4DZaP6y0qzD0I21fwtT4gZRItMFHYDJHZCpdeYDyLOX3Yx");
+            response.Headers.Add("X-Auth-Token", "d8KcVpyqLxtuA57QLKLA_88oSUFTqaOIdIUqWgnF4S3ialJu2sSQPqZfGRtpmMT5MvNXDUtp0jP-awY8cN4DZaP6y0qzD0I21fwtT4gZRItMFHYDJHZCpdeYDyLOX3Yx");
             response.EnsureSuccessStatusCode();
 
             var responseBytes = await response.Content.ReadAsByteArrayAsync();
